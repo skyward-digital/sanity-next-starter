3228cb97d209f7ced10c7dcdcab3e671600dcccc
Lucy Macartney <macartney.lucy@gmail.com>
2023-06-01 19:27:00 +0100

    Removes exit preview button

--- components/PagePreview/index.tsx
+++ components/PagePreview/index.tsx
@@ -1,5 +1,4 @@
 import PageComponents from "@/components/PageComponents";
-import Link from "next/link";
 
 import { usePreview } from "@/sanity/lib/sanity.preview";
 
@@ -11,15 +10,5 @@ type PagePreviewProps = {
 export default function PagePreview({ query, queryParams }: PagePreviewProps) {
   const pageData = usePreview(null, query, queryParams);
 
-  return (
-    <>
-      <PageComponents components={pageData[0]?.components || []} />
-      <Link
-        className="bg-blue-500 py-6 px-8 text-white font-bold fixed bottom-0 right-0 rounded-tl"
-        href="/api/exit-preview"
-      >
-        Exit Preview
-      </Link>
-    </>
-  );
+  return <PageComponents components={pageData[0]?.components || []} />;
 }
