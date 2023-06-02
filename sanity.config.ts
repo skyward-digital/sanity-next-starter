ce422dd24f626a56c5f3edfff561e7654929ebc7
Lucy Macartney <macartney.lucy@gmail.com>
2023-06-02 17:46:00 +0100

    Adds vision tool to Sanity Studio

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,6 +4,7 @@ import { deskTool } from "sanity/desk";
 import schemas from "@/sanity/schemas";
 import { apiVersion, dataset, projectId } from "@/sanity/lib/sanity.client";
 import { defaultDocumentNode } from "@/sanity/structure";
+import { visionTool } from "@sanity/vision";
 
 const config = defineConfig({
   projectId: projectId,
@@ -13,7 +14,9 @@ const config = defineConfig({
   // Where the Sanity Studio can be accessed
   // You can change this but you will need to change the /studio folder name in the /pages directory
   basePath: "/studio",
-  plugins: [deskTool({ defaultDocumentNode })],
+  // The vision tool can be configured to only be visible on dev and/or just for admin users
+  // Documentation: https://www.sanity.io/plugins/vision
+  plugins: [deskTool({ defaultDocumentNode }), visionTool()],
   schema: { types: schemas },
 });
 
